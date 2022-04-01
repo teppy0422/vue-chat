de4583021063aa6b785a55b7b228677769bd89eb
teppy0422 <teppy@aol.jp>
2022-04-02 06:00:54 +0900

    link for sidebar

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,19 @@
       <v-divider></v-divider>
 
       <v-list>
-        <v-list-item v-for="[icon, text] in links" :key="icon" link>
-          <router-link to="/">
-            <v-list-item-icon>
-              <v-icon>{{ icon }}</v-icon>
-            </v-list-item-icon>
+        <v-list-item
+          v-for="[icon, text, to] in links"
+          :key="icon"
+          :to="to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
 
-            <v-list-item-content>
-              <v-list-item-title>{{ text }}</v-list-item-title>
-            </v-list-item-content>
-          </router-link>
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -74,10 +77,10 @@ export default {
   data: () => ({
     drawer: null,
     links: [
-      ["mdi-inbox-arrow-down", "Inbox"],
-      ["mdi-send", "Send"],
-      ["mdi-delete", "Trash"],
-      ["mdi-alert-octagon", "Spam"],
+      ["mdi-inbox-arrow-down", "Inbox", "./"],
+      ["mdi-send", "Send", "/about"],
+      ["mdi-delete", "Trash", "about"],
+      ["mdi-alert-octagon", "Spam", "about"],
     ],
   }),
 };
